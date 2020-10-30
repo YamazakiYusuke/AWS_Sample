b9d30ba2aec946f23a974df614d6c46f494573fc
yamazakiyusuke <yusuke38098@gmail.com>
2020-10-30 00:33:41 +0000

    add
    i18nで英語、日本語対応　タイムゾーンをtokyoに設定

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -20,7 +20,7 @@ class TasksController < ApplicationController
 
     respond_to do |format|
       if @task.save
-        format.html { redirect_to @task, notice: '新しいタスクを登録しました' }
+        format.html { redirect_to tasks_path, notice: '新しいタスクを登録しました' }
         format.json { render :show, status: :created, location: @task }
       else
         format.html { render :new }
