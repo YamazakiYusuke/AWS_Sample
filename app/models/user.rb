21679b92e17476a6e66c3c123ea024281a766179
YamazakiYusuke <yusuke38098@gmail.com>
2020-11-09 02:35:45 +0000

    add
    ログイン機能追加　task/new, edit, showのデザイン実装

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  has_secure_password
+
+  validates :name, presence: true, length: { maximum: 30 }
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
+                    uniqueness: true
+  before_validation { email.downcase! }
+  validates :password, presence: true, length: { minimum: 8 }
+end
