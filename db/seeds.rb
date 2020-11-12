96b0bcfab44a4ea0ecfa43d3c38cdf278191eb95
YamazakiYusuke <yusuke38098@gmail.com>
2020-11-12 10:35:42 +0000

    fix
    seedデータの繰り返し数変更

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,7 +18,7 @@ users.each do |user|
   user_id << user.id
 end
 
-200.times do |n|
+1000.times do |n|
   id = user_id.sample
   title = ['テスト勉強する','カニ食べる','旅行に行く','買い物に行く'].sample
   style = ['一生懸命','コツコツと','時々','だらだらと'].sample
@@ -41,29 +41,7 @@ end
 
 
 # ['家庭','仕事','個人'].each do |n|
-#   Label.create
+#   Label.create!
 #     label: n
-#   end
-# end
-
-
-# tasks_id = []
-# tasks = User.all
-# tasks.each do |task|
-#   task_id << task.id
-# end
-
-# labels_id = []
-# labels = Label.all
-# labels.each do |label|
-#   label_id << label.id
-# end
-
-# 300.times do |n|
-#   task_id = labels_id.sample
-#   label_id = labels_id.sample
-#   JoinTaskLabel.create
-#     task_id: task_id,
-#     labels_id: label_id
 #   end
 # end
